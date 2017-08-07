a9043031c30906b9194a6d471e97410c8b5b6a76
Sharan-ram <sharanram1993@gmail.com>
2017-08-07 18:05:40 +0530

    first commit

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PhotoGrid = React.createClass({
+  render () {
+    return (
+      <div className='photo-grid'>
+        hello from photogrid
+      </div>
+    )
+  }
+})
+
+export default PhotoGrid
