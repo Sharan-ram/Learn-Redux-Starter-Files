4f8b8372bb2fbc188db4f21c10c7312756db1aaf
Sharan-ram <sharanram1993@gmail.com>
2017-08-07 18:59:36 +0530

    implement app component

--- learn-redux/client/components/App.js
+++ learn-redux/client/components/App.js
@@ -1,6 +1,7 @@
 import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 import * as actionCreators from '../actions/actionCreators'
+import Main from './Main'
 
 const mapStateToProps = (state) => {
   return {
@@ -13,6 +14,6 @@ const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(actionCreators, dispatch)
 }
 
-const App = connect(mapStateToProps, mapDispatchToProps)
+const App = connect(mapStateToProps, mapDispatchToProps)(Main)
 
 export default App
