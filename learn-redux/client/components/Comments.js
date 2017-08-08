079a655bcfb12bc00e1a627f005615e81851dc8b
Sharan-ram <sharanram1993@gmail.com>
2017-08-08 12:05:47 +0530

    render comments for each post

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
 const Comments = React.createClass({
+  renderComment(comment, i) {
+    return (
+      <div>
+        <p className='comment'>
+          <strong>{comment.user}:</strong> {comment.text}
+        </p>
+      </div>
+    )
+  },
+
   render () {
     return (
-      <div className='comment'>
-        i'm the comments
+      <div className='comments'>
+        {this.props.postComments.map(this.renderComment)}
+        <form ref='commentForm' className='comment-form'>
+          <input type='text' ref='author' placeholder='author' />
+          <input type='text' ref='comment' placeholder='comment' />
+          <input type='submit' hidden />
+        </form>
       </div>
     )
   }
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -5,11 +5,12 @@ import Comments from './Comments'
 const Single = React.createClass({
   render () {
     const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId)
+    const postComments = this.props.comments[this.props.params.postId] || []
 
     return (
       <div className='single-photo'>
         <Photo {...this.props} key={i} i={i} post={this.props.posts[i]} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     )
   }
